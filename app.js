08081455155ba3871a68a4304eddcef6c07c2140
Ahmer <ahmersajid80@gmail.com>
2025-07-06 19:14:27 +0500

    Js Updeted

--- app.js
+++ app.js
@@ -1,53 +1,47 @@
-let getHor = document.querySelectorAll('.h3')[0]
-let getMin = document.querySelectorAll('h3')[1];
-let getSec = document.querySelectorAll('h3')[2];
-let getMinSec = document.querySelectorAll('h3')[3];
+const getHor = document.querySelectorAll('h3')[0];
+const getMin = document.querySelectorAll('h3')[1];
+const getSec = document.querySelectorAll('h3')[2];
+const getMinSec = document.querySelectorAll('h3')[3];
+const startBtn = document.querySelector('#btn');
 
-let Hor = 0;
-let Min = 0;
-let Sec = 0;
-let MinSec = 0;
+let Hor = 0, Min = 0, Sec = 0, MinSec = 0;
 let interval;
 
 function start() {
-  interval = setInterval(function(){
-    MinSec++
-    getMinSec.innerHTML = MinSec
+  interval = setInterval(() => {
+    MinSec++;
+    getMinSec.innerHTML = MinSec;
     
-    if(MinSec >= 100){
-        Sec++
-        getSec.innerHTML = Sec
-        MinSec = 0
+    if (MinSec >= 100) {
+      Sec++;
+      getSec.innerHTML = Sec;
+      MinSec = 0;
     }
-    else if (Sec >= 60) {
-        Min++
-        getMin.innerHTML = Min
-        Sec = 0
+    if (Sec >= 60) {
+      Min++;
+      getMin.innerHTML = Min;
+      Sec = 0;
     }
-    else if (Min >= 60){
-        Hor++
-        getHor.innerHTML = Hor
-        Min = 0
+    if (Min >= 60) {
+      Hor++;
+      getHor.innerHTML = Hor;
+      Min = 0;
     }
-  },100)
-  document.querySelector("#btn").disabled = true
+  }, 10); 
+  startBtn.disabled = true;
 }
 
 function stop() {
- clearInterval(interval)
- document.querySelector('#btn').disabled = false
+  clearInterval(interval);
+  startBtn.disabled = false;
 }
 
 function reset() {
-    Hor = 0
-    Min = 0
-   Sec = 0
-   MinSec = 0
-   getMin.innerHTML = Hor
-   getMin.innerHTML = Min
-   getSec.innerHTML = Sec
-   getMinSec.innerHTML = MinSec
-   clearInterval(interval)
-   document.querySelector('#btn').disabled = false
+  Hor = Min = Sec = MinSec = 0;
+  getHor.innerHTML = Hor;
+  getMin.innerHTML = Min;
+  getSec.innerHTML = Sec;
+  getMinSec.innerHTML = MinSec;
+  clearInterval(interval);
+  startBtn.disabled = false;
 }
-
